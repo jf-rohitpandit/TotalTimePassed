2f8b487b183d7ad4ef779dea708c466f15dea168
rohit <rohit0311@outlook.com>
2020-10-09 10:21:30 +0530

    reset added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const time = require("./utils/loadTime.js");
-const saveTime = require("./utils/saveTime");
-const bodyParser = require("body-parser");
+const express = require('express');
+const time = require('./utils/loadTime.js');
+const saveTime = require('./utils/saveTime');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -15,33 +15,44 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    totalTime: {
-      hours: 0,
-      mins: 0,
-    },
-  });
-});
-
-app.get("/start", (req, res) => {
+app.get('/', (req, res) => {
   const pastData = time();
-  console.log("hel");
-  res.render("timer", {
+  res.render('index', {
     totalTime: pastData,
   });
 });
-app.post("/stop", (req, res) => {
-  console.log("hello");
-  if (req.body.hours === "") {
-    req.body.hours = "0";
+
+app.get('/start', (req, res) => {
+  console.log('start');
+  const pastData = time();
+  console.log('hel');
+  console.log(pastData);
+  res.render('timer', {
+    totalTime: pastData,
+  });
+});
+app.post('/stop', (req, res) => {
+  console.log('hello');
+  if (req.body.hours === '') {
+    req.body.hours = '0';
   }
-  if (req.body.mins === "") {
-    req.body.mins = "0";
+  if (req.body.mins === '') {
+    req.body.mins = '0';
   }
 
   saveTime(JSON.stringify(req.body));
-  res.redirect("/");
+  res.redirect('/');
+});
+
+app.post('/reset', (req, res) => {
+  console.log('reset');
+  const data = {
+    hours: 0,
+    mins: 0,
+  };
+  // console.log(JSON.stringify(data));
+  saveTime(JSON.stringify(data));
+  res.redirect('/');
 });
 
 app.listen(port, () => {
